44228f598c5cdd14f53791696fdd8dd0737c0a35
godofecht <abhishek.shivakumar@email.com>
2021-03-27 12:42:20 +0000

    updated readme

---
+++ zuma.py
@@ -0,0 +1,28 @@
+from os import walk
+import os
+import string
+from pathlib import Path
+
+
+
+import avel
+
+
+folder = os.getcwd()
+
+filenames = os.listdir(folder)
+filenames.remove("zuma.py")
+filenames.remove("zuma.exe")
+
+index = 0
+for filename in filenames:
+    if(os.path.exists(filename) == False):
+        print(os.path.exists(filename))
+        os.rename(filename,str(index)+".mp4")
+        index=index+1
+
+print(index)
+
+
+
+avel.combine_videos(filenames,"combined.mp4")
